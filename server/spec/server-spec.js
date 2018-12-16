3c4c3b7f66c8a508ca876e912ff5b0a581263787
John Doe <jason.andre.bourget@gmail.com>
2018-12-16 14:09:50 -0700

    Working on server-spec

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,7 +16,7 @@ describe('Persistent Node Chat Server', function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+       var tablename = 'messages'; // TODO: fill this out
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
@@ -65,10 +65,15 @@ describe('Persistent Node Chat Server', function() {
     });
   });
 
+  INSERT INTO table(c1,c2,...)
+VALUES (v1,v2,...);
+
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
+       var queryString = `INSERT INTO messages (text, id_users, id_rooms) VALUES ('Men like you can never change!', 1, 1)`;
        var queryArgs = [];
+       //check if room exists already, do not create another one
+
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
