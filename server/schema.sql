02796a90033aa4625832d6a5ebd925cb180e7cfe
John Doe <jason.andre.bourget@gmail.com>
2018-12-16 13:37:32 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,24 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(240),
+  id_users SMALLINT REFERENCES users(id),
+  id_rooms SMALLINT REFERENCES rooms(id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE rooms (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 /* Create other tables and define schemas for them here! */
