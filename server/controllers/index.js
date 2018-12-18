a516973f83e1a677afdc215502111792fcb224c8
John Doe <jason.andre.bourget@gmail.com>
2018-12-17 22:13:59 -0700

    Completed our models and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,37 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      models.messages.get((err,results)=>{
+        if(err) throw err;
+        return JSON.stringify(results);
+      });
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      //invoke post model
+      var {text, user, roomname} = req.body;
+      models.messages.post(text,user,roomname, (err)=>{
+        if(err) throw err;
+        console.log('MESSAGE POST SUCCESSFUL');
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get((err,results)=>{
+        if(err) throw err;
+        return JSON.stringify(results);
+      });
+    },
+    post: function (req, res) {
+      var user = req.body;
+      models.users.post(user, (err)=>{
+        if(err) throw err;
+        console.log('USER POST SUCCESSFUL');
+      });
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,61 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.connect((err)=>{
+        if(err) throw err;
+        console.log('connected in models get');
+        //bring data from all 3 tables, mess, rooms, users
+        var queryString = 'SELECT * FROM messages';
+        db.query(queryString, (err, result)=>{
+          if(err) throw err;
+          callback(result);
+        });
+      });
+    }, // a function which produces all the messages
+    post: function (text, user, roomName, callback) {
+      db.connect((err)=> {
+        if(err) throw err;
+        var queryArg =[text, user, roomName];
+        db.query('INSERT INTO rooms (name) VALUES (?)',roomName, (err)=>{
+          if(err)  throw err;
+          db.query('INSERT INTO users (name) VALUES (?)',user, (err)=>{
+            if(err)  throw err;
+            var queryString = 'INSERT INTO messages (text, id_users, id_rooms) VALUES (?,(SELECT id FROM users WHERE name = ?),(SELECT id FROM rooms WHERE name = ?))';
+            var queryArg = [text, user, roomName];
+            db.query(queryString, queryArg, (err)=>{
+              if(err) throw err;
+              callback();
+            });
+          });
+        });
+      })
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      db.connect((err)=>{
+        if(err) throw err;
+        console.log('connected in models get');
+        //bring data from all 3 tables, mess, rooms, users
+        var queryString = 'SELECT * FROM users';
+        db.query(queryString, (err, result)=>{
+          if(err) throw err;
+          callback(result);
+        });
+      });
+    },
+    post: function (user, callback) {
+      db.connect((err)=>{
+        db.query('INSERT INTO users (name) VALUES (?)',user, (err)=>{
+          if(err)  throw err;
+            callback();
+          });
+      });
+    }
   }
 };
 
